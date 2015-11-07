d513c339c1082ffe509d919901f4cbd25b427304
Bill Tran <bjtran1234@gmail.com>
2015-11-07 01:33:53 -0800

    added checked done

--- app/assets/javascripts/react_components/_todo.js.jsx
+++ app/assets/javascripts/react_components/_todo.js.jsx
@@ -1,9 +1,15 @@
 var Todo = React.createClass({
 	checkBoxClick: function(e){
 		if(e.currentTarget.checked){
+			var jsonObject = {todo:{done:true}};
+
+			this.props.updateData(jsonObject,"todos",this.props.todo.id, this.props.index)
 			this.refs.todoText.style.textDecoration = "line-through";
 			this.refs.todoText.style.opacity = "0.3";
 		}else{
+			var jsonObject = {todo:{done:false}};
+
+			this.props.updateData(jsonObject,"todos",this.props.todo.id, this.props.index)
 			this.refs.todoText.style.textDecoration = "";
 			this.refs.todoText.style.opacity = "1.0";
 		}
@@ -37,13 +43,16 @@ var Todo = React.createClass({
 		var jsonObject = {todo: {active:false}}
 		this.props.updateData(jsonObject, "todos", this.props.todo.id, this.props.index);
 	},
+	isDone: function(){
+		return this.props.todo.done ? {textDecoration : "line-through", opacity : "0.3" } : {}
+	},
   render: function () {
 
     return (
       <li className="todo">
-        <input type="checkbox" onClick={this.checkBoxClick} />
+        <input type="checkbox" onClick={this.checkBoxClick} defaultChecked={this.props.todo.done}/>
         <input type="text" ref="todoInput" defaultValue={this.props.todo.todo} onBlur={this.updateEntry} onKeyUp={this.updateEntry} />
-        <span ref="todoText" onClick={this.editTodo}>{this.props.todo.todo}</span>
+        <span ref="todoText" onClick={this.editTodo} style={this.isDone()}>{this.props.todo.todo}</span>
         <div className="delete" onClick={this.destroyEntry}>x</div>
       </li>
     );
